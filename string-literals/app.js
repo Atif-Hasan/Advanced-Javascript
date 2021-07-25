4d46f98c83bdccc76b9ccd0010ecfdb887395cff
Atif Hasan <atifhasansiddiqui@gmail.com>
2021-07-25 20:45:40 +0500

    Add files via upload

--- string-literals/app.js
+++ string-literals/app.js
@@ -9,3 +9,18 @@ carName = 'Civic';
 let carStatement =  `This is a ${carName} car`;
 
 console.log(carStatement);
+
+// Eg. 2
+
+window.onload = function(){
+
+    var str = 'braverrryyy';
+    console.log(str.repeat(50));
+    console.log(str.startswith('braverrryyy'));
+    console.log(str.endswith('braverrryyy'));
+    console.log(str.includes('c'));
+
+}
+
+
+
